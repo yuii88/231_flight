24cf33e0fa10a81318d749eb84e246eaa7c491ae
yuii88 <pim8828@gmail.com>
2021-11-06 23:15:22 +0700

    create Home and show on website

--- firstweb/myapp/urls.py
+++ firstweb/myapp/urls.py
@@ -1,7 +1,8 @@
 #urls.py
 from django.urls import path, include 
+from .views import Home # .views จากไฟล์ที่อยู่ในโฟรเดอร์เดียวกัน
 
 urlpatterns = [
-    path('',),
+    path('',Home),
 
 ]
--- firstweb/myapp/views.py
+++ firstweb/myapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# HttpResponse คือ ฟังก์ชั่นสำหรับทำให้โชว์ข้อความหน้าเว็บได้
 
-# Create your views here.
+def Home(request):
+    return HttpResponse('hello hong')
